d8b69ea0d79a2db34d24fa587c6303ba64c74a7b
asdcccct65 <desdhdj@gmail.com>
2025-06-17 12:55:18 -0400

    Deploy built React app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Welcome to your lovable.dev project.</p>
+    </div>
+  );
+}
+
+export default App;

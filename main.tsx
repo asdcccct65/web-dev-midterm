82735a45aa738c6f51c881f6c22cad13614e04b6
asdcccct65 <desdhdj@gmail.com>
2025-07-29 13:17:13 -0400

    Re-upload after deletion

--- main.tsx
+++ main.tsx
@@ -1,11 +1,13 @@
-
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Import this
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* ✅ Wrap your app here */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
